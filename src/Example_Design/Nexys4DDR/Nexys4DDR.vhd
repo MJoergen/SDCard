15bdf604a0e04438f4150e97baa697694680cdbe
Michael Jørgensen <mfj@silicom.dk>
2022-06-22 14:58:34 +0200

    Add support for Nexys4DDR board

--- src/Example_Design/Nexys4DDR/Nexys4DDR.vhd
+++ src/Example_Design/Nexys4DDR/Nexys4DDR.vhd
@@ -1,21 +1,28 @@
--- Main testbench for the SDCard controller.
--- This closely mimics the MEGA65 top level file, except that
--- clocks are generated directly, instead of via MMCM.
---
--- Created by Michael Jørgensen in 2022 (mjoergen.github.io/SDCard).
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity tb is
-end entity tb;
+entity top is
+   port (
+      clk_i          : in    std_logic;
+      btnl_i         : in    std_logic;
+      btnr_i         : in    std_logic;
+      btnu_i         : in    std_logic;
+      btnd_i         : in    std_logic;
+      btnc_i         : in    std_logic;
+      sd_cd_i        : in    std_logic;
+      sd_clk_o       : out   std_logic;
+      sd_cmd_io      : inout std_logic;
+      sd_dat_io      : inout std_logic_vector(3 downto 0);
+      uart_rx_i      : in    std_logic;
+      uart_tx_o      : out   std_logic
+   );
+end entity top;
 
-architecture simulation of tb is
+architecture synthesis of top is
 
-   -- Avalon Memory Map
    signal avm_clk           : std_logic;
-   signal avm_rst           : std_logic;  -- Synchronous reset, active high
+   signal avm_rst           : std_logic;
    signal avm_write         : std_logic;
    signal avm_read          : std_logic;
    signal avm_address       : std_logic_vector(31 downto 0);
@@ -24,8 +31,6 @@ architecture simulation of tb is
    signal avm_readdata      : std_logic_vector(7 downto 0);
    signal avm_readdatavalid : std_logic;
    signal avm_waitrequest   : std_logic;
-
-   -- SDCard device interface
    signal sd_clk            : std_logic;
    signal sd_cmd_in         : std_logic;
    signal sd_cmd_out        : std_logic;
@@ -34,33 +39,24 @@ architecture simulation of tb is
    signal sd_dat_out        : std_logic_vector(3 downto 0);
    signal sd_dat_oe         : std_logic;
 
-   -- Tristate
-   signal sd_cd             : std_logic;
-   signal sdClk             : std_logic;
-   signal cmd               : std_logic;
-   signal dat               : std_logic_vector(3 downto 0);
-
-   component sdModel is
-      port (
-         sdClk : in    std_logic;
-         cmd   : inout std_logic;
-         dat   : inout std_logic_vector(3 downto 0)
-      );
-   end component sdModel;
-
-   signal uart_tx           : std_logic;
+   signal reset_n           : std_logic;
 
 begin
 
+   reset_n <= not btnc_i;
+
+
    ---------------------------------------------------------
-   -- Generate clock and reset
+   -- Instantiate clock generator
    ---------------------------------------------------------
 
-   i_tb_clk : entity work.tb_clk
+   i_clk : entity work.clk
       port map (
-         clk_o => avm_clk,
-         rst_o => avm_rst
-      ); -- i_tb_clk
+         sys_clk_i  => clk_i,
+         sys_rstn_i => reset_n,
+         clk_o      => avm_clk,
+         rst_o      => avm_rst
+      ); -- i_clk
 
 
    ---------------------------------------------------------
@@ -98,7 +94,7 @@ begin
          avm_readdata_o      => avm_readdata,
          avm_readdatavalid_o => avm_readdatavalid,
          avm_waitrequest_o   => avm_waitrequest,
-         sd_cd_i             => sd_cd,
+         sd_cd_i             => sd_cd_i,
          sd_clk_o            => sd_clk,
          sd_cmd_in_i         => sd_cmd_in,
          sd_cmd_out_o        => sd_cmd_out,
@@ -106,7 +102,7 @@ begin
          sd_dat_in_i         => sd_dat_in,
          sd_dat_out_o        => sd_dat_out,
          sd_dat_oe_o         => sd_dat_oe,
-         uart_tx_o           => uart_tx
+         uart_tx_o           => uart_tx_o
       ); -- i_sdcard_wrapper
 
 
@@ -114,24 +110,11 @@ begin
    -- Connect I/O buffers
    ---------------------------------------------------------
 
-   sdClk     <= sd_clk;
-   sd_cmd_in <= cmd;
-   sd_dat_in <= dat;
-   cmd <= sd_cmd_out when sd_cmd_oe = '1' else 'Z';
-   dat <= sd_dat_out when sd_dat_oe = '1' else (others => 'Z');
+   sd_clk_o  <= sd_clk;
+   sd_cmd_in <= sd_cmd_io;
+   sd_dat_in <= sd_dat_io;
+   sd_cmd_io <= sd_cmd_out when sd_cmd_oe = '1' else 'Z';
+   sd_dat_io <= sd_dat_out when sd_dat_oe = '1' else (others => 'Z');
 
-
-   ---------------------------------------------------------
-   -- Instantiate SDCard simulation model
-   ---------------------------------------------------------
-
-   sd_cd <= '0';
-   i_sdModel : sdModel
-      port map (
-         sdClk => sdClk,
-         cmd   => cmd,
-         dat   => dat
-      ); -- i_sdModel
-
-end architecture simulation;
+end architecture synthesis;
 
---
+++ src/Example_Design/Nexys4DDR/Nexys4DDR.xdc
@@ -0,0 +1,43 @@
+# Signal mapping for Nexys4DDR platform
+#
+# Created by Michael Jørgensen in 2022 (mjoergen.github.io/SDCard).
+
+
+# Place SD close to I/O pins
+create_pblock pblock_i_sd
+add_cells_to_pblock pblock_i_sd [get_cells [list i_sdcard_wrapper/i_sdcard_cmd_logger/i_sdcard_cmd]]
+resize_pblock pblock_i_sd -add {SLICE_X82Y113:SLICE_X89Y136}
+
+## External clock signal (100 MHz)
+set_property -dict {PACKAGE_PIN E3 IOSTANDARD LVCMOS33} [get_ports clk_i]
+create_clock -period 10.000 -name clk [get_ports clk_i]
+
+# Buttons
+set_property -dict {PACKAGE_PIN P17 IOSTANDARD LVCMOS33} [get_ports btnl_i]
+set_property -dict {PACKAGE_PIN M17 IOSTANDARD LVCMOS33} [get_ports btnr_i]
+set_property -dict {PACKAGE_PIN M18 IOSTANDARD LVCMOS33} [get_ports btnu_i]
+set_property -dict {PACKAGE_PIN P18 IOSTANDARD LVCMOS33} [get_ports btnd_i]
+set_property -dict {PACKAGE_PIN N17 IOSTANDARD LVCMOS33} [get_ports btnc_i]
+
+## Micro SD Connector (external slot at back of the cover)
+set_property -dict {PACKAGE_PIN A1 IOSTANDARD LVCMOS33} [get_ports sd_cd_i]
+set_property -dict {PACKAGE_PIN B1 IOSTANDARD LVCMOS33} [get_ports sd_clk_o]
+set_property -dict {PACKAGE_PIN C1 IOSTANDARD LVCMOS33} [get_ports sd_cmd_io]
+set_property -dict {PACKAGE_PIN C2 IOSTANDARD LVCMOS33 PULLUP true} [get_ports sd_dat_io[0]]
+set_property -dict {PACKAGE_PIN E1 IOSTANDARD LVCMOS33 PULLUP true} [get_ports sd_dat_io[1]]
+set_property -dict {PACKAGE_PIN F1 IOSTANDARD LVCMOS33 PULLUP true} [get_ports sd_dat_io[2]]
+set_property -dict {PACKAGE_PIN D2 IOSTANDARD LVCMOS33 PULLUP true} [get_ports sd_dat_io[3]]
+
+## USB-RS232 Interface (rxd, txd only; rts/cts are not available)
+set_property -dict {PACKAGE_PIN C4 IOSTANDARD LVCMOS33} [get_ports uart_rx_i]
+set_property -dict {PACKAGE_PIN D4 IOSTANDARD LVCMOS33} [get_ports uart_tx_o]
+
+## Configuration and Bitstream properties
+set_property CONFIG_VOLTAGE 3.3 [current_design]
+set_property CFGBVS VCCO [current_design]
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
+set_property BITSTREAM.CONFIG.CONFIGRATE 66 [current_design]
+set_property CONFIG_MODE SPIx4 [current_design]
+set_property BITSTREAM.CONFIG.SPI_32BIT_ADDR YES [current_design]
+set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]
+
